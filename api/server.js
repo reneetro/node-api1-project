f2b03123163960d9ad6775917337b2a2e2806570
renee.troiani@gmail.com <renee.troiani@gmail.com>
2022-06-06 13:43:17 -0600

    get hadnlers complete

--- api/server.js
+++ api/server.js
@@ -1,7 +1,30 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
+const User = require('./users/model');
 
 const server = express();
+
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    User.find().then(result => {
+        res.json(result)
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id).then(result => {
+        if(result == null){
+            res.status(404).json({ message: 'does not exist'});
+            return;
+        }
+        res.json(result)
+    })
+})
+
+
+
+
 
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
